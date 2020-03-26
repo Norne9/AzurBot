eaf6794d420ad975ff8eb0a14dccae19d1db4a9a
Norne9 <aleksey191295@gmail.com>
2020-03-26 19:20:38 +0500

    Better triangle detection

--- enemy_finder.py
+++ enemy_finder.py
@@ -9,16 +9,22 @@ def find_triangles(screen: np.ndarray, yellow: bool) -> List[Tuple[int, int]]:
     screen = screen & np.uint8(0b11000000)
 
     if yellow:
-        target_bgr = np.array([[[128, 192, 192]]], dtype=np.uint8)  # yellow
+        target_bgr1 = np.array([[[128, 192, 192]]], dtype=np.uint8)  # yellow
+        target_bgr2 = np.array([[[192, 192, 192]]], dtype=np.uint8)  # white
     else:
-        target_bgr = np.array([[[128, 128, 192]]], dtype=np.uint8)  # red
+        target_bgr1 = np.array([[[128, 128, 192]]], dtype=np.uint8)  # red
+        target_bgr2 = np.array([[[64, 64, 192]]], dtype=np.uint8)  # red red
 
-    screen = (screen - target_bgr).sum(axis=2)
-    screen[screen != 0] = 255
+    screen1 = (screen - target_bgr1).sum(axis=2)
+    screen2 = (screen - target_bgr2).sum(axis=2)
+
+    screen = np.full((screen.shape[0], screen.shape[1]), 0)
+    screen[(screen1 != 0) & (screen2 != 0)] = 255
+    screen[Img.deadzone_image < 100] = 255
     screen = screen.astype(np.uint8)
 
     res = cv2.matchTemplate(screen, Img.triangle_template, cv2.TM_SQDIFF_NORMED)
-    loc = np.where(res < 0.33)
+    loc = np.where(res < 0.18)
     locks = zip(*loc[::-1])
 
     result = []
